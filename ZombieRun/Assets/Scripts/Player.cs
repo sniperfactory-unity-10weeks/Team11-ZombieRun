eff52d4c97ace487efd74a759512fde101f44262
3tPepper <3tpepperok@gmail.com>
2023-10-12 13:28:38 +0900

    좀비 공격 구현

---
+++ ZombieRun/Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float playerHP = 10f; // 시작 체력 
+    public float health { get; protected set; }
+
+    void Start()
+    {
+        // 체력을 시작체력으로 초기화
+        health = playerHP;
+    }
+
+    // 대미지를 입는 기능
+    public void OnDamage(float damage)
+    {
+        //아직 죽지 않았다면 체력 감소 처리 실행
+        if (!GameManager.instance.isGameOver)
+        {
+            // 대미지만큼 체력 감소
+            playerHP -= damage;
+            //만약 체력이 0 이하라면, 사망
+            if (playerHP <= 0)
+            {
+                Die();
+            }
+        }
+    }
+
+
+    // 사망 처리
+    private void Die()
+    {
+        // 사망 상태를 참으로 변경
+        GameManager.instance.isGameOver = true;
+    }
+}
--- ZombieRun/Assets/Scripts/ZombieMove.cs
+++ ZombieRun/Assets/Scripts/ZombieMove.cs
@@ -9,6 +9,12 @@ public class ZombieMove : MonoBehaviour
     private NavMeshAgent pathFinder;
     private Animator zombieAnimator;
 
+    public float damage = 2f; // 공격력
+    private float timeBetAttack = 0.5f; //공격 간격
+    private float lastAttackTime;       //최근 공격 시간
+
+
+
     void Start()
     {
 
@@ -41,5 +47,30 @@ public class ZombieMove : MonoBehaviour
 
         pathFinder.isStopped = true;
         zombieAnimator.SetBool("isWalk", false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //플레이어가 게임오버라면, 실행하지 않음
+        if (GameManager.instance.isGameOver)
+        {
+            return;
+        }
+        // 최근 공격 시점에서 timeBetAttack 이상 시간이 지났다면 공격 가능
+        if (Time.time >= lastAttackTime + timeBetAttack)
+        {
+            // 상대방으로부터 LivingEntity 타입을 가져오기 시도
+            Player attackTarget = other.GetComponent<Player>();
+
+            // 닿은 대상이 플레이어라면,
+            if (attackTarget != null && other == target)
+            {
+                // 최근 공격 시간을 갱신
+                lastAttackTime = Time.time;
+
+                // 공격 실행
+                attackTarget.OnDamage(damage);
+            }
+        }
     }
 }
