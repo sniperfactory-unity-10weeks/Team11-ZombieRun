ee2a853d33b8cc68ca9a3773329cb5cb1c130492
JeongSooHyeon <jsh75721@naver.com>
2023-10-12 23:32:06 +0900

    HUD modify

--- ZombieRun/Assets/Scripts/UIManager.cs
+++ ZombieRun/Assets/Scripts/UIManager.cs
@@ -95,4 +95,17 @@ public class UIManager : MonoBehaviour
         AttackedEffectPanel.SetActive(false);
     }
 
+    
+    // GameOver //
+    // 재시작 버튼 클릭, 메인 Scene 로드
+    public void LoadMain()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    // 나가기 버튼 클릭, 시작 Scene 로드
+    public void Exit()
+    {
+        SceneManager.LoadScene("Start");
+    }
 }
