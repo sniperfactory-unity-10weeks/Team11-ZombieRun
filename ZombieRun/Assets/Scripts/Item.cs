e4f84d39080c971aa974bb3fba8711b06d4c9d77
3tPepper <3tpepperok@gmail.com>
2023-10-13 13:36:14 +0900

    엔딩 연결

--- ZombieRun/Assets/Scripts/Item.cs
+++ ZombieRun/Assets/Scripts/Item.cs
@@ -17,13 +17,22 @@ public class Item : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
+
             GameManager.instance.inventory[ItemNum] = true;   // 아이템 획득 처리
             
             // 해당 아이템에 대한 UI 업데이트
             UIManager.instance.UpdateKeys(ItemNum);
 
-            // 아이템 획득 시 소리 재생
-            KeyDrop.Play();
+            if (ItemNum == (int)GameManager.EItem.Ring)
+            {
+                // 아이템 획득 시 소리 재생
+                KeyDrop.Play();
+            }
+            else
+            {
+
+            }
+
             gameObject.SetActive(false);                  // 아이템 비활성화
         }
     }
--- ZombieRun/Assets/Scripts/ZombieMove.cs
+++ ZombieRun/Assets/Scripts/ZombieMove.cs
@@ -20,18 +20,18 @@ public class ZombieMove : MonoBehaviour
 
         //테스트 위해 코루틴을 여기서 실행시켰습니다.
         ////이후 방1에서 열쇠 획득 시 활성화되도록 변경 예정
-        target = GameObject.FindWithTag("Player");
-        pathFinder = GetComponent<NavMeshAgent>();
-        zombieAnimator = GetComponent<Animator>();
-        StartCoroutine(UpdatePath());
-    }
-
-    private void OnEnable()
-    {
 /*        target = GameObject.FindWithTag("Player");
         pathFinder = GetComponent<NavMeshAgent>();
         zombieAnimator = GetComponent<Animator>();
         StartCoroutine(UpdatePath());*/
+    }
+
+    private void OnEnable()
+    {
+        target = GameObject.FindWithTag("Player");
+        pathFinder = GetComponent<NavMeshAgent>();
+        zombieAnimator = GetComponent<Animator>();
+        StartCoroutine(UpdatePath());
     }
 
     private IEnumerator UpdatePath()
